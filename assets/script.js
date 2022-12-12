29ff5d0383b3b534338c72cc88dc43fe53b4cd15
Chris Wilson <120229536+Cwilson84@users.noreply.github.com>
2022-12-12 05:11:02 -0500

    Revert "Added advanced JavaScript functionality"

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,17 @@
 let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-let randomTopic = topics[Math.floor(Math.random() * topics.length)];
-
-function listTopics() {
-    for (var x = 0; x < topics.length; x++) {
-        console.log(topics[x])
-    }
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
 }
-
-function selectTopic() {
-if (randomTopic === 'HTML') {
+/*
+if (topic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (randomTopic === 'CSS') { 
+} else if (topic === 'CSS') { 
     console.log("Let's study CSS!");
-} else if (randomTopic === 'Git') {
+} else if (topic === 'Git') {
     console.log("Let's study Git");
-} else if (randomTopic === 'JavaScript') {
+} else if (topic === 'JavaScript') {
     console.log("Let's study JavaScript!");
 } else {
     console.log("Please try again!");
-    }
 }
-
-console.log("Here are the topics covered through Prework: ")
-listTopics()
-console.log("Which topic should we study first?")
-selectTopic()
+*/
